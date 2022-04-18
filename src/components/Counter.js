e9d1eeaec7eb16fa65e94108ca7aeb387399f8cc
Marco <marco_trabajo@yahoo.com>
2022-04-18 18:10:48 +0200

    one test, one successfully completed exercise using useReducer with folder structure

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,29 +1,34 @@
 import React, { useReducer } from 'react'
+import { counterReducer, initialState } from '../counterReducer/counterReducer'
+import { TYPES } from '../counterActions/counterActions';
+// import from counterActions/counterActions.js -----------
+// const TYPES = {
+//    INCREMENT: 'INCREMENT',
+//    INCREMENT_5: 'INCREMENT_5',
+//    DECREMENT: 'DECREMENT',
+//    RESET: 'RESET',
+// }
+// --------------------------------------------------------
 
-const TYPES = {
-   INCREMENT: 'INCREMENT',
-   INCREMENT_5: 'INCREMENT_5',
-   DECREMENT: 'DECREMENT',
-   RESET: 'RESET',
-}
-
-const initialState = {
-   count: 0,
-}
-
-function reducer(state, action) {
-   switch (action.type) {
-      case TYPES.INCREMENT: 
-         return { count: state.count + 1 }
-      case TYPES.INCREMENT_5: 
-         return { count: state.count + action.payload}
-      default: 
-         return state;
-   }
-}
-
+// import from counterReducer/counterReducer.js ----------
+// const initialState = {
+   //    count: 0,
+   // }
+   
+   // function reducer(state, action) {
+      //    switch (action.type) {
+         //       case TYPES.INCREMENT: 
+         //          return { count: state.count + 1 }
+         //       case TYPES.INCREMENT_5: 
+         //          return { count: state.count + action.payload}
+         //       default: 
+         //          return state;
+         //    }
+         // }
+         
+// --------------------------------------------------------
 const Counter = () => {
-   const [state, dispatch] = useReducer(reducer, initialState);
+   const [state, dispatch] = useReducer(counterReducer, initialState);
    const add = () => dispatch({type: TYPES.INCREMENT});
    const add5 = () => dispatch({type: TYPES.INCREMENT_5, payload: 5});
   return (
